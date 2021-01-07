79bfaac4c5fcc3ff1c6180cfb2e7dcc414f7881f
Nicolas LOUBOUTIN <nicolas.louboutin@st.com>
2021-01-07 18:05:17 +0100

    st-android-11.0.0-2021-01-29 (v2.0.0)

--- HealthImpl.cpp
+++ HealthImpl.cpp
@@ -13,18 +13,40 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define LOG_TAG "android.hardware.health@2.0-service.stm32mp1"
 
-#include <health2/Health.h>
-#include <health2/service.h>
-#include <healthd/healthd.h>
+#define LOG_TAG "android.hardware.health@2.1-impl.stm32mp1"
 
-#include <android-base/file.h>
+#include <memory>
+#include <string_view>
 
 #include <android-base/logging.h>
+#include <android-base/file.h>
 
-using android::hardware::health::V2_0::DiskStats;
-using android::hardware::health::V2_0::StorageInfo;
+#include <health/utils.h>
+#include <health2impl/Health.h>
+#include <hidl/Status.h>
+
+#include <hal_conversion.h>
+
+using ::android::sp;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::hardware::health::InitHealthdConfig;
+using ::android::hardware::health::V2_1::IHealth;
+using ::android::hidl::base::V1_0::IBase;
+using ::android::hardware::health::V2_0::DiskStats;
+using ::android::hardware::health::V2_0::Result;
+
+using ::android::hardware::health::V1_0::hal_conversion::convertFromHealthInfo;
+using ::android::hardware::health::V1_0::hal_conversion::convertToHealthInfo;
+
+using namespace std::literals;
+
+namespace android {
+namespace hardware {
+namespace health {
+namespace V2_1 {
+namespace implementation {
 
 static constexpr char kBatteryStatsFile[] = "/sys/class/power_supply/";
 
@@ -35,17 +57,51 @@ static constexpr char kUsbChargerName[] = "dummy-charger-usb_c";
 static constexpr size_t kDiskStatsSize = 11;
 
 #ifdef EMMC_STORAGE
-static constexpr char kDiskName[] = "mmcblk1";
-static constexpr char kDiskStatsFile[] = "/sys/block/mmcblk1/stat";
+static constexpr char kBootDeviceName[] = "mmcblk1";
+static constexpr char kBootDeviceStatsFile[] = "/sys/block/mmcblk1/stat";
 #else
-static constexpr char kDiskName[] = "mmcblk0";
-static constexpr char kDiskStatsFile[] = "/sys/block/mmcblk0/stat";
+static constexpr char kBootDeviceName[] = "mmcblk0";
+static constexpr char kBootDeviceStatsFile[] = "/sys/block/mmcblk0/stat";
 #endif
 
-void healthd_board_init(struct healthd_config*) {}
+// android::hardware::health::V2_1::implementation::Health implements most
+// defaults.
+class HealthImpl : public Health {
+  public:
+    HealthImpl(std::unique_ptr<healthd_config>&& config)
+        : Health(std::move(config)) {}
+    // Return<void> getChargeCounter(getChargeCounter_cb _hidl_cb) override;
+    // Return<void> getCurrentNow(getCurrentNow_cb _hidl_cb) override;
+    // Return<void> getCurrentAverage(getCurrentAverage_cb _hidl_cb) override;
+    // Return<void> getCapacity(getCapacity_cb _hidl_cb) override;
+    // Return<void> getEnergyCounter(getEnergyCounter_cb _hidl_cb) override;
+    // Return<void> getChargeStatus(getChargeStatus_cb _hidl_cb) override;
+    // Return<void> getStorageInfo(getStorageInfo_cb _hidl_cb) override;
+    Return<void> getDiskStats(getDiskStats_cb _hidl_cb) override;
+    // Return<void> getHealthInfo(getHealthInfo_cb _hidl_cb) override;
 
-int healthd_board_battery_update(
-    struct android::BatteryProperties* battery_props) {
+    // Functions introduced in Health HAL 2.1.
+    // Return<void> getHealthConfig(getHealthConfig_cb _hidl_cb) override;
+    // Return<void> getHealthInfo_2_1(getHealthInfo_2_1_cb _hidl_cb) override;
+    // Return<void> shouldKeepScreenOn(shouldKeepScreenOn_cb _hidl_cb) override;
+
+  protected:
+    void UpdateHealthInfo(HealthInfo* health_info) override;
+
+  private:
+    bool BoardBatteryUpdate(BatteryProperties* battery_props);
+};
+
+void HealthImpl::UpdateHealthInfo(HealthInfo* health_info) {
+  struct BatteryProperties props;
+
+  convertFromHealthInfo(health_info->legacy.legacy, &props);
+  bool res = BoardBatteryUpdate(&props);
+  if (res)
+    convertToHealthInfo(&props, health_info->legacy.legacy);
+}
+
+bool HealthImpl::BoardBatteryUpdate(BatteryProperties* battery_props) {
 
   std::string buffer;
   std::string file, battery;
@@ -59,7 +115,7 @@ int healthd_board_battery_update(
   file = file.append("technology");
   if (!android::base::ReadFileToString(file, &buffer)) {
     LOG(ERROR) << file << ": ReadFileToString failed.";
-    return 1;
+    return false;
   }
   battery_props->batteryTechnology = buffer.c_str();
 
@@ -67,7 +123,7 @@ int healthd_board_battery_update(
   file = file.append("capacity");
   if (!android::base::ReadFileToString(file, &buffer)) {
     LOG(ERROR) << file << ": ReadFileToString failed.";
-    return 1;
+    return false;
   }
   battery_props->batteryLevel = std::stoi(buffer);
 
@@ -75,7 +131,7 @@ int healthd_board_battery_update(
   file = file.append("current_max");
   if (!android::base::ReadFileToString(file, &buffer)) {
     LOG(ERROR) << file << ": ReadFileToString failed.";
-    return 1;
+    return false;
   }
   battery_props->maxChargingCurrent = std::stoi(buffer);
 
@@ -83,7 +139,7 @@ int healthd_board_battery_update(
   file = file.append("current_now");
   if (!android::base::ReadFileToString(file, &buffer)) {
     LOG(ERROR) << file << ": ReadFileToString failed.";
-    return 1;
+    return false;
   }
   battery_props->batteryCurrent = std::stoi(buffer);
 
@@ -91,7 +147,7 @@ int healthd_board_battery_update(
   file = file.append("voltage_max");
   if (!android::base::ReadFileToString(file, &buffer)) {
     LOG(ERROR) << file << ": ReadFileToString failed.";
-    return 1;
+    return false;
   }
   battery_props->maxChargingVoltage = std::stoi(buffer);
 
@@ -100,7 +156,7 @@ int healthd_board_battery_update(
   file = file.append("voltage_now");
   if (!android::base::ReadFileToString(file, &buffer)) {
     LOG(ERROR) << file << ": ReadFileToString failed.";
-    return 1;
+    return false;
   }
   battery_props->batteryVoltage = std::stoi(buffer) / 1000;
 
@@ -108,7 +164,7 @@ int healthd_board_battery_update(
   file = file.append("temp");
   if (!android::base::ReadFileToString(file, &buffer)) {
     LOG(ERROR) << file << ": ReadFileToString failed.";
-    return 1;
+    return false;
   }
   // read value in units of 0.1°C, returned value in °C
   battery_props->batteryTemperature = std::stoi(buffer) / 10;
@@ -117,7 +173,7 @@ int healthd_board_battery_update(
   file = file.append("cycle_count");
   if (!android::base::ReadFileToString(file, &buffer)) {
     LOG(ERROR) << file << ": ReadFileToString failed.";
-    return 1;
+    return false;
   }
   battery_props->batteryCycleCount = std::stoi(buffer);
 
@@ -125,7 +181,7 @@ int healthd_board_battery_update(
   file = file.append("charge_full");
   if (!android::base::ReadFileToString(file, &buffer)) {
     LOG(ERROR) << file << ": ReadFileToString failed.";
-    return 1;
+    return false;
   }
   battery_props->batteryFullCharge = std::stoi(buffer);
 
@@ -134,7 +190,7 @@ int healthd_board_battery_update(
   file = file.append("charge_counter");
   if (!android::base::ReadFileToString(file, &buffer)) {
     LOG(ERROR) << file << ": ReadFileToString failed.";
-    return 1;
+    return false;
   }
   battery_props->batteryChargeCounter = std::stoi(buffer);
 
@@ -142,7 +198,7 @@ int healthd_board_battery_update(
   file = std::string(kBatteryStatsFile).append(std::string(kAcChargerName)).append("/online");
   if (!android::base::ReadFileToString(file, &buffer)) {
     LOG(ERROR) << file << ": ReadFileToString failed.";
-    return 1;
+    return false;
   }
   battery_props->chargerAcOnline = buffer != "0";
 
@@ -150,7 +206,7 @@ int healthd_board_battery_update(
   file = std::string(kBatteryStatsFile).append(std::string(kUsbChargerName)).append("/online");
   if (!android::base::ReadFileToString(file, &buffer)) {
     LOG(ERROR) << file << ": ReadFileToString failed.";
-    return 1;
+    return false;
   }
   battery_props->chargerUsbOnline = buffer != "0";
 
@@ -162,7 +218,7 @@ int healthd_board_battery_update(
   file = file.append("status");
   if (!android::base::ReadFileToString(file, &buffer)) {
     LOG(ERROR) << file << ": ReadFileToString failed.";
-    return 1;
+    return false;
   }
 
   // remove last space
@@ -184,7 +240,7 @@ int healthd_board_battery_update(
   file = file.append("health");
   if (!android::base::ReadFileToString(file, &buffer)) {
     LOG(ERROR) << file << ": ReadFileToString failed.";
-    return 1;
+    return false;
   }
 
   // remove last space
@@ -220,33 +276,56 @@ int healthd_board_battery_update(
             << "batteryChargeCounter = " << battery_props->batteryChargeCounter << "\n"
             << "batteryTechnology = " << battery_props->batteryTechnology;
 
-  return 0;
+  return true;
 }
 
-void get_storage_info(std::vector<struct StorageInfo>&) {}
+Return<void> HealthImpl::getDiskStats(getDiskStats_cb _hidl_cb) {
+  std::vector<struct DiskStats> stats;
 
-void get_disk_stats(std::vector<struct DiskStats>& vec_stats) {
-  DiskStats stats = {};
+  // Integrate only the boot device stats by default
+  DiskStats bootDeviceStat = {};
 
-  stats.attr.isInternal = true;
-  stats.attr.isBootDevice = true;
-  stats.attr.name = std::string(kDiskName);
+  bootDeviceStat.attr.isInternal = true;
+  bootDeviceStat.attr.isBootDevice = true;
+  bootDeviceStat.attr.name = std::string(kBootDeviceName);
 
   std::string buffer;
-  if (!android::base::ReadFileToString(std::string(kDiskStatsFile), &buffer)) {
-    LOG(ERROR) << kDiskStatsFile << ": ReadFileToString failed.";
-    return;
+  if (!android::base::ReadFileToString(std::string(kBootDeviceStatsFile), &buffer)) {
+    LOG(ERROR) << kBootDeviceStatsFile << ": ReadFileToString failed.";
+    _hidl_cb(Result::NOT_SUPPORTED, {});
+    return Void();
   }
 
-  // Regular diskstats entries
   std::stringstream ss(buffer);
   for (uint i = 0; i < kDiskStatsSize; i++) {
-    ss >> *(reinterpret_cast<uint64_t*>(&stats) + i);
+    ss >> *(reinterpret_cast<uint64_t*>(&bootDeviceStat) + i);
   }
-  vec_stats.resize(1);
-  vec_stats[0] = stats;
 
-  return;
+  stats.resize(1);
+  stats[0] = bootDeviceStat;
+
+  hidl_vec<struct DiskStats> stats_vec(stats);
+  if (!stats.size()) {
+    _hidl_cb(Result::NOT_SUPPORTED, {});
+  } else {
+    _hidl_cb(Result::SUCCESS, stats_vec);
+  }
+  return Void();
 }
 
-int main(void) { return health_service_main(); }
+}  // namespace implementation
+}  // namespace V2_1
+}  // namespace health
+}  // namespace hardware
+}  // namespace android
+
+extern "C" IHealth* HIDL_FETCH_IHealth(const char* instance) {
+    using ::android::hardware::health::V2_1::implementation::HealthImpl;
+    if (instance != "default"sv) {
+        return nullptr;
+    }
+    auto config = std::make_unique<healthd_config>();
+    InitHealthdConfig(config.get());
+
+    return new HealthImpl(std::move(config));
+}
